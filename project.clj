d7c892d16019c9daaf356456faed1d420b9b5d2a
danielle kefford <danielle_kefford@hotmail.com>
2015-06-03 14:24:56 -0400

    Had to change project.clj due to changes in Leiningen.

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject mixing-java-and-clojure "1.0.0-SNAPSHOT"
   :description "A simple demonstration of Java interop and build management using javac"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :source-path "src/clojure"
-  :java-source-path "src/java"
-  :java-options {:debug "true"}
-  :main com.quephird.mjac.bar)
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"]
+  :main com.quephird.mjac.bar
+  :aot [com.quephird.mjac.bar])
